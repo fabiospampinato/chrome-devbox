bf0b032fce4a00ac5ee90cb2f2ebc45330c05501
Fabio Spampinato <spampinabio@gmail.com>
2025-07-30 20:13:49 +0100

    Added some shared utils

---
+++ src/shared/utils.ts
@@ -0,0 +1,19 @@
+
+/* IMPORT */
+
+import isEqual from 'are-deeply-equal';
+
+/* MAIN */
+
+const isTruthy = <T> ( value: T ): value is Exclude<T, 0 | -0 | 0n | -0n | '' | false | null | undefined | void> => {
+
+  return !!value;
+
+};
+
+/* EXPORT */
+
+export {
+  isEqual,
+  isTruthy,
+};
