b8e452399ff8d660a65fcb62cdff9f5682249d1d
Fabio Spampinato <spampinabio@gmail.com>
2025-08-17 14:01:57 +0100

    New tool: Animation Throttler, currently disabled as it doesn't work for some reason

---
+++ src/content/tools/animation_throttler.ts
@@ -0,0 +1,27 @@
+
+/* IMPORT */
+
+import useToolTrigger from '@hooks/use_tool_trigger';
+import Debugger from '@lib/debugger';
+
+/* MAIN */
+
+//FIXME: Figure out why this thing doesn't work...
+
+const AnimationThrottler: ToolConfig = {
+  id: 'animation-throttler',
+  name: 'Animation Throttler',
+  description: 'Toggle 5x animation speed throttling, to spot issues with animations in the page',
+  command: 'devbox.animation-throttler.toggle',
+  shortcut: 'Ctrl+Cmd+A',
+  trigger: useToolTrigger ( (): Disposer => {
+    Debugger.callInWorker ( 'Animation.setPlaybackRate', { playbackRate: 0.2 }, 1 );
+    return (): void => {
+      Debugger.callInWorker ( 'Animation.setPlaybackRate', { playbackRate: 1 }, -1 );
+    };
+  })
+};
+
+/* EXPORT */
+
+export default AnimationThrottler;
