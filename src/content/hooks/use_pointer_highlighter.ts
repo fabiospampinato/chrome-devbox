07ec5d84ece623fb47cd6fff515c1b1abfccfa8a
Fabio Spampinato <spampinabio@gmail.com>
2025-09-14 21:14:56 +0100

    PointerHighlighter: added support for rendering the pressed button too

--- src/content/hooks/use_pointer_highlighter.ts
+++ src/content/hooks/use_pointer_highlighter.ts
@@ -1,4 +1,3 @@
-
 /* IMPORT */
 
 import {$} from 'voby';
@@ -9,7 +8,13 @@ import useResolved from '@hooks/use_resolved';
 
 /* TYPES */
 
+type Angles = {
+  start: number,
+  end: number
+};
+
 type Point = {
+  button: number,
   x: number,
   y: number
 };
@@ -18,18 +23,39 @@ type Point = {
 
 const CIRCLE_RADIUS = 22;
 const CIRCLE_THICKNESS = 1.5;
+
+const CIRCLE_BUTTON_ARC_LENGTH = 0.35 * Math.PI;
+const CIRCLE_BUTTON_ARC_GAP = 0.1 * Math.PI;
+
 const COLOR_BLACK = '#220A4F';
 const COLOR_WHITE = '#FFFFFF';
 
+const RENDER_BUTTON = true;
+
+const CIRCLE_BUTTON_ARC_ANGLES: Partial<Record<number, Angles>> = {
+  0: { // left
+    start: 1.1 * Math.PI,
+    end: 1.1 * Math.PI + CIRCLE_BUTTON_ARC_LENGTH,
+  },
+  1: { // middle
+    start: 1.5 * Math.PI - ( CIRCLE_BUTTON_ARC_LENGTH / 2 ),
+    end: 1.5 * Math.PI + ( CIRCLE_BUTTON_ARC_LENGTH / 2 ),
+  },
+  2: { // right
+    start: 1.9 * Math.PI - CIRCLE_BUTTON_ARC_LENGTH,
+    end: 1.9 * Math.PI,
+  },
+};
+
 const event2point = ( event: MouseEvent ): Point => {
+  const button = event.button;
   const x = event.clientX;
   const y = event.clientY;
-  return { x, y };
+  return { button, x, y };
 };
 
 /* MAIN */
 
-//TODO: Make which button was pressed visible
 //TODO: Make modifier keys visible (Shift, Ctrl, Alt, Meta)
 //TODO: Support multiple pointers
 
@@ -49,12 +75,19 @@ const usePointerHighlighter = ( ref: $<Element | undefined> = document.body ): v
 
     point ( prev => prev && event2point ( event ) );
 
-
   });
 
   useEventListener ( window, 'mouseup', ( event: MouseEvent ) => {
 
     point ( undefined );
+
+  });
+
+  useEventListener ( window, 'contextmenu', ( event: MouseEvent ) => {
+
+    setTimeout ( () => {
+      point ( undefined );
+    }, 50 );
 
   });
 
@@ -68,23 +101,65 @@ const usePointerHighlighter = ( ref: $<Element | undefined> = document.body ): v
 
       if ( !point ) return;
 
-      const {x, y} = point;
+      const {button, x, y} = point;
 
-      /* PAINTING BLACK CIRCLE */
+      if ( RENDER_BUTTON && button >= 0 && button <= 2 ) {
 
-      ctx.beginPath ();
-      ctx.arc ( x, y, CIRCLE_RADIUS, 0, 2 * Math.PI, false );
-      ctx.lineWidth = CIRCLE_THICKNESS;
-      ctx.strokeStyle = COLOR_BLACK;
-      ctx.stroke ();
+        const buttonAngles = CIRCLE_BUTTON_ARC_ANGLES[button];
 
-      /* PAINTING WHITE CIRCLE */
+        if ( !buttonAngles ) return;
 
-      ctx.beginPath ();
-      ctx.arc ( x, y, CIRCLE_RADIUS - CIRCLE_THICKNESS, 0, 2 * Math.PI, false );
-      ctx.lineWidth = CIRCLE_THICKNESS;
-      ctx.strokeStyle = COLOR_WHITE;
-      ctx.stroke ();
+        const paintCircleArcs = ( radius: number, color: string ) => {
+
+          ctx.lineWidth = CIRCLE_THICKNESS;
+          ctx.strokeStyle = color;
+
+          /* PAINTING BUTTON SEGMENT */
+
+          const buttonStartAngle = buttonAngles.start;
+          const buttonEndAngle = buttonAngles.end;
+
+          ctx.beginPath ();
+          ctx.arc ( x, y, radius, buttonStartAngle, buttonEndAngle, false );
+          ctx.stroke ();
+
+          /* PAINTING BODY SEGMENT */
+
+          const bodyStartAngle = buttonEndAngle + CIRCLE_BUTTON_ARC_GAP;
+          const bodyEndAngle = buttonStartAngle - CIRCLE_BUTTON_ARC_GAP + ( 2 * Math.PI );
+
+          ctx.beginPath ();
+          ctx.arc ( x, y, radius, bodyStartAngle, bodyEndAngle, false );
+          ctx.stroke ();
+
+        };
+
+        /* PAINTING SEGMENTED CIRCLES */
+
+        paintCircleArcs ( CIRCLE_RADIUS, COLOR_BLACK );
+        paintCircleArcs ( CIRCLE_RADIUS - CIRCLE_THICKNESS, COLOR_WHITE );
+
+      } else {
+
+        const paintCircle = ( radius: number, color: string ): void => {
+
+          ctx.lineWidth = CIRCLE_THICKNESS;
+          ctx.strokeStyle = color;
+
+          /* PAINTING CIRCLE */
+
+          ctx.beginPath ();
+          ctx.arc ( x, y, radius, 0, 2 * Math.PI, false );
+          ctx.stroke ();
+
+        };
+
+        /* PAINTING FULL CIRCLES */
+
+        paintCircle ( CIRCLE_RADIUS, COLOR_BLACK );
+        paintCircle ( CIRCLE_RADIUS - CIRCLE_THICKNESS, COLOR_WHITE );
+
+      }
 
     });
 
